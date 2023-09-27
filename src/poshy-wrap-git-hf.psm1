76cf9dc387b1ff0247a77ead5ec672fd292a4f4a
Bruce Markham <219281+brucificus@users.noreply.github.com>
2023-09-27 18:50:00 -0400

    ♻ Makes Module Exports Explicit

--- src/poshy-wrap-git-hf.psm1
+++ src/poshy-wrap-git-hf.psm1
@@ -18,31 +18,34 @@ function Invoke-GitHubflow {
     & $git_bin hf @args
 }
 Set-Alias -Name ghf -Value Invoke-GitHubflow
+Export-ModuleMember -Function Invoke-GitHubflow -Alias ghf
 
 function Invoke-GitHubflowFeature {
     & $git_bin hf feature @args
 }
 Set-Alias -Name ghff -Value Invoke-GitHubflowFeature
+Export-ModuleMember -Function Invoke-GitHubflowFeature -Alias ghff
 
 function Invoke-GitHubflowRelease {
     & $git_bin hf release @args
 }
 Set-Alias -Name ghfr -Value Invoke-GitHubflowRelease
+Export-ModuleMember -Function Invoke-GitHubflowRelease -Alias ghfr
 
 function Invoke-GitHubflowHotfix {
     & $git_bin hf hotfix @args
 }
 Set-Alias -Name ghfh -Value Invoke-GitHubflowHotfix
+Export-ModuleMember -Function Invoke-GitHubflowHotfix -Alias ghfh
 
 function Invoke-GitHubflowSupport {
     & $git_bin hf support @args
 }
 Set-Alias -Name ghfs -Value Invoke-GitHubflowSupport
+Export-ModuleMember -Function Invoke-GitHubflowSupport -Alias ghfs
 
 function Invoke-GitHubflowUpdate {
     & $git_bin hf update @args
 }
 Set-Alias -Name ghfu -Value Invoke-GitHubflowUpdate
-
-
-Export-ModuleMember -Function * -Alias *
+Export-ModuleMember -Function Invoke-GitHubflowUpdate -Alias ghfu
